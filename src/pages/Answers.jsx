d50436aaaa0a9aa7906e68de928a74ee3dc1a232
Teboho Makhalemele <tebohomakhalemele@outlook.com>
2022-08-23 10:09:10 +0200

    space betwix cards

--- src/pages/Answers.jsx
+++ src/pages/Answers.jsx
@@ -27,7 +27,7 @@ function Answer(props) {
    </div>);
 
    let answer2 = (
-      <div className="card w-50"   >
+      <div className="card w-75"   >
          <div className="card-body">
          <h5 className="card-title">Answer to {quester}'s question</h5>
             <p className="card-text">Q: {questionText}</p>
