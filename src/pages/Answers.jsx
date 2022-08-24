975ff80d70338b36ece1ccf57f3d7a802ccd0267
Teboho Makhalemele <tebohomakhalemele@outlook.com>
2022-08-25 00:05:56 +0200

    seconds

--- src/pages/Answers.jsx
+++ src/pages/Answers.jsx
@@ -52,7 +52,7 @@ function Answers() {
       <div className="container text-primary" role="status">
          <h2 className="">Checking if he's answered any questions...
          <br />
-         <small class="text-muted">It could be that the internet is slow: give it 20-is seconds</small>
+         <small class="text-muted">It could be that the internet is slow: give it 15-ish seconds</small>
          </h2>
       </div>
    );
